7683defd5bc912649eb3487594ba2b6f47a69f30
anton dahlberg <anton.m.dahlberg@gmail.com>
2020-12-04 11:39:14 +0200

    pistooli, joka ampuu ja lataa

---
+++ Assets/script/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float leftLimit = -40;
+    public float rightlimit = 40;
+
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.x > rightlimit)
+
+            Destroy(gameObject);
+    }
+
+
+
+
+
+
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            
+
+
+        }
+
+    }
+}
