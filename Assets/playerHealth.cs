6c063f0c3b2580c78fde83b3026738ef1abd058d
anton dahlberg <anton.m.dahlberg@gmail.com>
2020-12-18 12:12:42 +0200

    shoot player

---
+++ Assets/playerHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerHealth : MonoBehaviour
+{
+    public int Health;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "bullet1")
+        {
+            if (Health <= 0)
+            {
+
+                Destroy(gameObject);
+
+                Youlose();
+
+            }
+
+
+
+
+
+
+
+        }
+
+    }
+    public void Youlose()
+    {
+        SceneManager.LoadScene("you lose");
+    }
+}
