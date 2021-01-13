b4407a237c0c260a888acd598a172d59f8ba4e83
anton dahlberg <anton.m.dahlberg@gmail.com>
2021-01-13 11:02:21 +0200

    juoksee, kävelee, menee kyykkyyn ja vihollinen ampuu

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,27 @@ public class PlayerMovement : MonoBehaviour
 
 {
     public CharacterController controller;
-    public float speed = 25f;
+    public float speed;
     public float gravity = -20f;
     public Transform groundCheck;
     public float groundDistance = 0.5f;
     public LayerMask groundMask;
     public float jumpHeight = 3.0f;
+    public float crouchSpeed;
+    public float crouchWalkSpeed;
+    public bool isCrouching = false;
+    public bool isCrouched = false;
+
 
     Vector3 velocity;
     bool isGrounded;
+    
     // Update is called once per frame
+
     void Update()
     {
+        Crouch();
+        IsCrouchingVoid();
         //checks if player is on ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -40,12 +49,67 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); 
 
         }
+        if (Input.GetKeyDown("left shift"))
+        {
+            speed = 15;
+        }
+        if (Input.GetKeyUp("left shift"))
+        {
+            speed = 5;
+        }
+        
 
-        //gravity 
-        velocity.y += gravity * Time.deltaTime;
+
+            //gravity 
+            velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
     }
+    public void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) && isGrounded)
+        {
+            isCrouched = !isCrouched;
+            isCrouching = true;
+        }
+    }
+
+    public void IsCrouchingVoid()
+    {
+        if (!isCrouched)
+        {
+            if (isCrouching)
+            {
+                Vector3 temp = transform.localScale;
+                temp.y = 0.5f;
+                temp.x = 0.5f;
+                temp.z = 0.5f;
+                transform.localScale = temp;
+
+                if (temp.y <= 0.5f)
+                {
+                    temp.y = 0.5f;
+                    isCrouching = false;
+                }
+            }
+        }
+
+        else if (isCrouched)
+        {
+            if (isCrouching)
+            {
+                Vector3 temp = transform.localScale;
+                temp.y = 0.25f;
+                temp.x = 0.25f;
+                temp.z = 0.25f;
+                transform.localScale = temp;
+
+                
+            }
+        }
+    }
+
+
 
 
 }
